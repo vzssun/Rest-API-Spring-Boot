ae91ab125ad8edce25244111f457f04ae06e8bfa
Igor Bueno <igorsbueno010@gmail.com>
2026-05-18 20:31:08 -0300

    feat: Add Swagger documentation and update security configurations

--- src/main/java/restapi/spring/project/Controller/AuthenticationController.java
+++ src/main/java/restapi/spring/project/Controller/AuthenticationController.java
@@ -3,6 +3,10 @@ package restapi.spring.project.Controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import restapi.spring.project.Dto.request.LoginRequest;
 import restapi.spring.project.Dto.request.LogoutRequest;
 import restapi.spring.project.Dto.request.RegisterRequest;
@@ -11,7 +15,7 @@ import restapi.spring.project.Services.AuthenticationService;
 import restapi.spring.project.Services.JwtService;
 
 import java.util.Optional;
-
+@Tag(name = "Authentication", description = "Registro, login e logout com blacklist de token JWT")
 @RestController
 @RequestMapping("api/auth")
 public class AuthenticationController {
@@ -22,11 +26,17 @@ public class AuthenticationController {
         this.authenticationService = authenticationService;
 
     }
+    @Operation(summary = "Registra um novo usuário")
+    @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Dados inválidos para registro")
     @PostMapping("/signup")
     public ResponseEntity<RegisterRequest> register(@RequestBody RegisterRequest request) {
         RegisterRequest register = authenticationService.createUser(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(register);
     }
+    @Operation(summary = "Realiza login e retorna JWT")
+    @ApiResponse(responseCode = "202", description = "Login realizado, token retornado")
+    @ApiResponse(responseCode = "401", description = "Credenciais inválidas")
     @PostMapping("/login")
     public ResponseEntity<Optional<LoginResponse>> authenticate(@RequestBody LoginRequest request) {
         Optional<LoginResponse> response = Optional.ofNullable(authenticationService.authenticate(request));
@@ -34,6 +44,8 @@ public class AuthenticationController {
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
 
     }
+    @Operation(summary = "Realiza logout e revoga o token", description = "Adiciona o token na blacklist do cache. Token se torna inválido imediatamente mesmo antes de expirar")
+    @ApiResponse(responseCode = "200", description = "Logout realizado com sucesso")
     @DeleteMapping("/logout")
     public ResponseEntity<LogoutRequest> logout(@RequestBody LogoutRequest request) {
         LogoutRequest logout = authenticationService.logout(request);
--- src/main/java/restapi/spring/project/Controller/UserController.java
+++ src/main/java/restapi/spring/project/Controller/UserController.java
@@ -2,6 +2,10 @@ package restapi.spring.project.Controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -11,7 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import restapi.spring.project.Model.UserModel;
 import restapi.spring.project.Services.UserService;
 
-
+@Tag(name = "Users", description = "Gerenciamento de usuários")
 @RestController
 @RequestMapping("api/users")
 public class UserController {
@@ -19,6 +23,8 @@ public class UserController {
     @Autowired
    private UserService userService;
 
+    @Operation(summary = "Lista usuários com paginação")
+    @ApiResponse(responseCode = "200", description = "Usuários retornados com sucesso")
     @GetMapping
     //@PreAuthorize("hasAnyAuthority('admin:read', 'management:read')")
   public Page<UserModel> getUsers(
